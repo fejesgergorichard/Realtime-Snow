620b31ed5a3515c2404e0cfa9924867588079dee
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-02-24 14:23:41 +0100

    Modified to multiple objects

--- Assets/WheelImpressions.cs
+++ Assets/WheelImpressions.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class WheelImpressions : MonoBehaviour {
 
     public Shader _shader;
-    public GameObject _terrain;
+    public GameObject[] _terrains;
     public Transform[] _wheels;
-
     [Range(0, 2)]
     public float _bSize;
 
@@ -20,34 +19,52 @@ public class WheelImpressions : MonoBehaviour {
     private RaycastHit _hit;
     private int _mask;
 
+    private Collider _currentPlane;
     // Use this for initialization
     void Start () {
         _mask = LayerMask.GetMask("Ground");
 
         _draw = new Material(_shader);
-        _snow = _terrain.GetComponent<MeshRenderer>().material; // tesselation shader
-        _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
-        _snow.SetTexture("_Splatmap", _splatmap);
+
+        for (int i = 0; i < _terrains.Length; i++)
+        {
+            _snow = _terrains[i].GetComponent<MeshRenderer>().material;
+            _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+            _snow.SetTexture("_Splatmap", _splatmap);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        for (int i = 0; i < _wheels.Length; i++)
+        for (int k = 0; k < _terrains.Length; k++)
         {
-            // raycasting towards mesh
-            if (!Physics.Raycast(_wheels[i].position, - Vector3.up, out _hit, 1f, _mask))
+            for (int i = 0; i < _wheels.Length; i++)
             {
-                continue;
+                // raycasting towards mesh
+                if (!Physics.Raycast(_wheels[i].position, -Vector3.up, out _hit, 1f, _mask))
+                {
+                    continue;
+                }
+
+                _draw.SetVector("_Coordinates", new Vector4(_hit.textureCoord.x, _hit.textureCoord.y, 0, 0));
+                _draw.SetFloat("_Strength", _bStrength);
+                _draw.SetFloat("_Size", _bSize);
+                RenderTexture tmp = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(_splatmap, tmp);
+                Graphics.Blit(tmp, _splatmap, _draw);
+                RenderTexture.ReleaseTemporary(tmp);
             }
+        }
+    }
 
-            _draw.SetVector("_Coordinates", new Vector4(_hit.textureCoord.x, _hit.textureCoord.y, 0, 0));
-            _draw.SetFloat("_Strength", _bStrength);
-            _draw.SetFloat("_Size", _bSize);
-            RenderTexture tmp = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
-            Graphics.Blit(_splatmap, tmp);
-            Graphics.Blit(tmp, _splatmap, _draw);
-            RenderTexture.ReleaseTemporary(tmp);
-
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider != _currentPlane)
+        {
+            _currentPlane = other.collider;
+            _snow = _currentPlane.GetComponent<MeshRenderer>().material;
+            _splatmap = (RenderTexture)_snow.GetTexture("_Splatmap");
+            _snow.SetTexture("_Splatmap", _splatmap);
         }
     }
 }
